ea92d5babd8584f0be3fc1aa7e921b0055f12856
guido-egea <126272468+guido-egea@users.noreply.github.com>
2024-03-28 21:25:29 -0300

    Delete Entrega final Guido Egea  Agustin Rey directory

--- Entrega final Guido Egea  Agustin Rey/clinica-odontologica/src/main/java/com/backend/clinicaodontologica/repository/TurnoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.clinicaodontologica.repository;
-
-import com.backend.clinicaodontologica.entity.Turno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TurnoRepository extends JpaRepository<Turno, Long> {
-}
